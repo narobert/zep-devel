5d1bbd76ecca1a1a10a3a946a1c50be313398d2f
Ubuntu <ubuntu@ip-10-0-0-141.us-west-2.compute.internal>
2016-05-02 09:11:13 +0000

    added Playlist table to dev database

--- vkconfig/models.py
+++ vkconfig/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Playlist(models.Model):
+
+    id = models.AutoField('#', primary_key = True)
+    user = models.ForeignKey(User)
+    playlist_name = models.CharField(max_length = 100)
+    total_duration = models.IntegerField()
+    num_songs = models.IntegerField()
